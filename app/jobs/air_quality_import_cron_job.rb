93c40fed2b56182177244a18b884d32e6cc8a87f
Akshay Birajdar <akshaybirajdar05@gmail.com>
2025-02-05 20:02:31 +0530

    Import task for inital seed

--- app/jobs/air_quality_import_cron_job.rb
+++ app/jobs/air_quality_import_cron_job.rb
@@ -1,10 +1,10 @@
 class AirQualityImportCronJob < ApplicationJob
   queue_as :importer
 
-  def perform(*args)
+  def perform(from = nil, to = nil)
     # get all data for past 1 hour
-    from = 1.hour.ago
-    to = Time.current
+    from ||= 1.hour.ago
+    to ||= Time.current
 
     jobs = Location.in_batches(of: 10).map do |relation|
       AirQualityImportJob.new(relation.pluck(:id), from, to)
